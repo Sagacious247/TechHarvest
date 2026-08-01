d992fee1989db6d73ae626dd415289b3fc479be3
sagacious paul <princesagacious96@gmail.com>
2026-08-02 00:08:42 +0100

    Test module dashboard route

--- backend/src/routes/moduleDashboard.routes.ts
+++ backend/src/routes/moduleDashboard.routes.ts
@@ -1,34 +1,49 @@
+// import { Router } from "express";
+
+// import asyncHandler
+// from "../middlewares/asyncHandler";
+
+// import {
+//   authenticate,
+// } from "../middlewares/authenticate";
+
+// import {
+//   authorize,
+// } from "../middlewares/authorize";
+
+// import {
+//   getModuleDashboardController,
+// } from "../controllers/moduleDashboard.controller";
+
+// const router = Router();
+
+// router.get(
+//   "/:courseId",
+//   authenticate,
+//   authorize(
+//     "super_admin",
+//     "admin"
+//   ),
+
+//   asyncHandler(
+//     getModuleDashboardController
+//   )
+
+// );
+
+// export default router;
+
+
+
 import { Router } from "express";
-
-import asyncHandler
-from "../middlewares/asyncHandler";
-
-import {
-  authenticate,
-} from "../middlewares/authenticate";
-
-import {
-  authorize,
-} from "../middlewares/authorize";
-
-import {
-  getModuleDashboardController,
-} from "../controllers/moduleDashboard.controller";
 
 const router = Router();
 
-router.get(
-  "/:courseId",
-  authenticate,
-  authorize(
-    "super_admin",
-    "admin"
-  ),
-
-  asyncHandler(
-    getModuleDashboardController
-  )
-
-);
+router.get("/:courseId", (req, res) => {
+  res.json({
+    success: true,
+    message: "NEW MODULE DASHBOARD ROUTE IS RUNNING",
+  });
+});
 
 export default router;
